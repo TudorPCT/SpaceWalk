4824de20f7aa48e2385f99ad4c52276ea7766f86
iacobdariaroxana <iacobdariaroxana@gmail.com>
2022-12-01 21:05:05 +0200

    update launcher scene and game manager

--- SpaceWalk/Assets/Scripts/GameManager.cs
+++ SpaceWalk/Assets/Scripts/GameManager.cs
@@ -23,21 +23,9 @@ public class GameManager : MonoBehaviourPunCallbacks
 
     #region Public Methods
 
-
     public void LeaveRoom()
     {
         PhotonNetwork.LeaveRoom();
-    }
-
-    void LoadArena()
-    {
-        if (!PhotonNetwork.IsMasterClient)
-        {
-            Debug.LogError("PhotonNetwork : Trying to Load a level but we are not the master Client");
-            return;
-        }
-        Debug.LogFormat("PhotonNetwork : Loading Level : {0}", PhotonNetwork.CurrentRoom.PlayerCount);
-        PhotonNetwork.LoadLevel("Room for " + PhotonNetwork.CurrentRoom.PlayerCount);
     }
 
     #endregion
@@ -54,8 +42,6 @@ public class GameManager : MonoBehaviourPunCallbacks
         {
             Debug.LogFormat("OnPlayerEnteredRoom IsMasterClient {0}", PhotonNetwork.IsMasterClient); // called before OnPlayerLeftRoom
 
-
-            LoadArena();
         }
     }
 
@@ -69,8 +55,6 @@ public class GameManager : MonoBehaviourPunCallbacks
         {
             Debug.LogFormat("OnPlayerLeftRoom IsMasterClient {0}", PhotonNetwork.IsMasterClient); // called before OnPlayerLeftRoom
 
-
-            LoadArena();
         }
     }
 
