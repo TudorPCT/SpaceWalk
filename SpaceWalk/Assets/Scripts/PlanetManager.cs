adf4ec8d83dde3272961b13bc983f3d5930eb439
iacobdariaroxana <iacobdariaroxana@gmail.com>
2022-12-17 19:04:05 +0200

    voice chat works

---
+++ SpaceWalk/Assets/Scripts/PlanetManager.cs
@@ -0,0 +1,48 @@
+using Photon.Pun;
+using UnityEngine;
+using com.unity.photon;
+using Photon.Realtime;
+
+public class PlanetManager : MonoBehaviourPunCallbacks
+{
+    public GameObject _character;
+    public GameObject _spawn;
+    public static PlanetManager Instance;
+    void Start()
+    {
+        Instance = this;
+        if (PlayerManager.LocalPlayerInstance == null && PhotonNetwork.IsMasterClient)
+        {
+            PhotonNetwork.Instantiate(_character.name, _spawn.transform.position, Quaternion.identity, 0);
+        }
+        else
+        {
+        }
+    }
+    public override void OnPlayerEnteredRoom(Player other)
+    {
+        Debug.LogFormat("OnPlayerEnteredRoom() {0}", other.NickName); // not seen if you're the player connecting
+
+
+        if (PhotonNetwork.IsMasterClient)
+        {
+            Debug.LogFormat("OnPlayerEnteredRoom IsMasterClient {0}", PhotonNetwork.IsMasterClient); // called before OnPlayerLeftRoom
+        }
+    }
+
+
+    public override void OnPlayerLeftRoom(Player other)
+    {
+        Debug.LogFormat("OnPlayerLeftRoom() {0}", other.NickName); // seen when other disconnects
+
+
+        if (PhotonNetwork.IsMasterClient)
+        {
+            Debug.LogFormat("OnPlayerLeftRoom IsMasterClient {0}", PhotonNetwork.IsMasterClient); // called before OnPlayerLeftRoom
+        }
+    }
+    void Update()
+    {
+
+    }
+}
---
+++ SpaceWalk/Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,63 @@
+using Photon.Pun;
+using UnityEngine;
+
+namespace com.unity.photon
+{
+    /// <summary>
+    /// Player manager.
+    public class PlayerManager : MonoBehaviourPunCallbacks
+    {
+        #region Public Fields
+
+        [Tooltip("The local player instance. Use this to know if the local player is represented in the Scene")]
+        public static GameObject LocalPlayerInstance;
+
+        #endregion
+
+
+        #region MonoBehaviour CallBacks
+
+        /// MonoBehaviour method called on GameObject by Unity during early initialization phase.
+        void Awake()
+        {
+            // #Important
+            // used in GameManager.cs: we keep track of the localPlayer instance to prevent instantiation when levels are synchronized
+            if (photonView.IsMine)
+            {
+                LocalPlayerInstance = this.gameObject;
+            }
+            // #Critical
+            // we flag as don't destroy on load so that instance survives level synchronization, thus giving a seamless experience when levels load.
+            DontDestroyOnLoad(this.gameObject);
+        }
+
+        void Start()
+        {
+            Debug.Log("added new player");
+        }
+
+        void Update()
+        {
+
+        }
+
+        void OnTriggerEnter(Collider other)
+        {
+            if (!photonView.IsMine)
+            {
+                return;
+            }
+        }
+
+        void OnTriggerStay(Collider other)
+        {
+            // we dont' do anything if we are not the local player.
+            if (!photonView.IsMine)
+            {
+                return;
+            }
+        }
+        #endregion
+
+    }
+}
