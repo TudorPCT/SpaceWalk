8c38cca3e517f8ebff64b4aa695c0bac2613af64
iacobdariaroxana <iacobdariaroxana@gmail.com>
2022-12-29 13:49:59 +0200

    add canvas for space scene

---
+++ SpaceWalk/Assets/Scripts/ChoosePlanet.cs
@@ -0,0 +1,41 @@
+using Photon.Pun;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChoosePlanet : MonoBehaviour
+{
+    public GameObject VisitButton;
+    public TMP_Dropdown dropdown;
+    private string _selectedOption;
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void Choose()
+    {
+        _selectedOption = dropdown.options[dropdown.value].text;
+        int sceneIndex = SceneUtility.GetBuildIndexByScenePath($"Assets/Scenes/{_selectedOption}.unity");
+        if (sceneIndex != -1)
+        {
+            Debug.Log($"Scene with name {_selectedOption} exists.");
+            VisitButton.SetActive(true);
+        }
+        else
+        {
+            Debug.Log($"Scene with name {_selectedOption} does not exist.");
+            VisitButton.SetActive(false);
+        }
+    }
+
+    public void Visit()
+    {
+        PhotonNetwork.LoadLevel(_selectedOption);
+    }
+}
